07d9180ddc9f2e80427881ca758a8c5aa2615c68
ElianTemesDev <eliantemes1303@gmail.com>
2023-04-30 08:03:56 -0300

    Version 2.0 of the n3 challenge, b) completed!

--- ThirdChallenge/TrainMadness.cs
+++ ThirdChallenge/TrainMadness.cs
@@ -8,38 +8,67 @@ namespace ThirdChallenge
         static void Main(string[] args)
         {
             const int cubicMetersCapacity = 141, totalWagonsCubicCapacity = cubicMetersCapacity * 3;
-            /*Mientras ninguna caja individual supere las constantes de largo, ancho y alto, se asume factibilidad, despues se analizan 
-            todas las cajas para ver que no superen la capacidad cubica total de los 3 vagones*/
-            double sumOfCubicMeters = 0;
+            double[] wagonsSize = { cubicMetersCapacity, cubicMetersCapacity, cubicMetersCapacity};
+            List<double> listOfBoxSizes = new();
             double[] boxValues = new double[3];
             bool impossibleBox = false, tooBigBoxFlag = false;
-            List<Box> boxes = new List<Box>();
+            List<Box> boxes = new();
 
             //Aca asumo que una caja jamas podria tener alguno de sus tres datos como cero por lo tanto esa caja no se toma como valida y se utilizan el resto
             AskForBoxes(boxValues, ref impossibleBox, ref boxes);
 
+            /*Mientras ninguna caja individual supere las constantes de largo, ancho y alto, se asume factibilidad, despues se analizan 
+            todas las cajas para ver que no superen la capacidad cubica total de los 3 vagones*/
             //Sumo los metros cubicos de las cajas y checkeo si alguna caja sobre pasa las dimensiones del tren
-            sumOfCubicMeters = SumCubicMetersOfBoxes(boxes, ref tooBigBoxFlag);
+            double sumOfCubicMeters = SumCubicMetersOfBoxes(boxes, ref tooBigBoxFlag, ref listOfBoxSizes);
 
-            //Se fija que ninguna caja supere las medidas del vagon o que la suma de todas las cajas sobrepase la capacidad total de los 3 vagones
-            CheckFactibility(totalWagonsCubicCapacity, sumOfCubicMeters, tooBigBoxFlag);
+            /* Se fija que ninguna caja supere las medidas del vagon o que la suma de todas las cajas sobrepase la capacidad total de los 3 vagones
+             despues se utiliza una heuristica de ubicar la caja en el vagon que tenga el maximo espacio disponible de los 3 */
+            if (CheckFactibility(totalWagonsCubicCapacity, sumOfCubicMeters, tooBigBoxFlag))
+            {
+                int[] quantityOfBoxesInWagon = new int [3];
+                int wagonIndex;
+                foreach (double cubicMeters in listOfBoxSizes) 
+                {
+                    double maxValue;
+                    maxValue = wagonsSize.Max();
+
+                    wagonIndex = Array.IndexOf(wagonsSize, maxValue);
+                    if (wagonsSize[wagonIndex] - cubicMeters > 0)
+                    {
+                        wagonsSize[wagonIndex] = wagonsSize[wagonIndex] - cubicMeters;
+                        quantityOfBoxesInWagon[wagonIndex]++;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"La caja de tamaño {cubicMeters} m3 no entra en ninguno de los tres vagones, se deja la caja y se sigue con las siguientes para ver si pueden entrar.");
+                    }
+                }
+                for(int i = 0; i < wagonsSize.Length; i++)
+                {
+                    Console.WriteLine($"Sobra de espacio en el vagon {i+1} {wagonsSize[i]} m3.");
+                    Console.WriteLine($"Cantidad de cajas en el vagon {quantityOfBoxesInWagon[i]}U.");
+                }
+            }
         }
 
-        private static void CheckFactibility(int totalWagonsCubicCapacity, double sumOfCubicMeters, bool tooBigBoxFlag)
+        private static bool CheckFactibility(int totalWagonsCubicCapacity, double sumOfCubicMeters, bool tooBigBoxFlag)
         {
             if (sumOfCubicMeters <= totalWagonsCubicCapacity && tooBigBoxFlag != true)
             {
                 Console.WriteLine($"Es factible guardar las cajas en los 3 vagones, estas ocuparian un total de {sumOfCubicMeters} m3");
+                return true;
             }
             else
             {
                 Console.WriteLine("No es factible guardar las cajas en los 3 vagones, estas superan la capacidad total de los 3 vagones.");
                 Console.WriteLine($"La capacidad total de los 3 vagones es de {totalWagonsCubicCapacity} m3");
                 Console.WriteLine($"Mientras que las cajas estan ocupando: {sumOfCubicMeters} m3");
+                return false;
             }
         }
 
-        private static double SumCubicMetersOfBoxes(List<Box> boxes, ref bool tooBigBoxFlag)
+        private static double SumCubicMetersOfBoxes(List<Box> boxes, ref bool tooBigBoxFlag, ref List<double> cubicMeterOfEachBox)
         {
             double accumulator = 0;
 
@@ -49,6 +78,8 @@ namespace ThirdChallenge
                 {
                     break;
                 }
+
+                cubicMeterOfEachBox.Add(boxes[i].ToCubicMeters());
                 accumulator += boxes[i].ToCubicMeters();
             }
 
@@ -56,7 +87,7 @@ namespace ThirdChallenge
         }
 
         private static bool CheckIfBoxOutOfBounds(Box box, ref bool tooBigBoxFlag)
-        {           
+        {
             if (!CanFitBox(box))
             {
                 Console.WriteLine("Es imposible realizar la carga de todas las cajas, la ");
